61f2830a2d813162886eb97bb9c60138e1dda37f
itanglishdev <chrishethertonweb@gmail.com>
2022-10-25 21:17:52 +0200

    added CharBar style and props

--- src/components/chart/ChartBar.js
+++ src/components/chart/ChartBar.js
@@ -1,9 +1,20 @@
-import Chart from '/Chart'
+import "./CharBar.css";
 
 const ChartBar = (props) => {
-  return (
-    <div>ChartBar</div>
-  )
-}
+  let barFillHeight = "0%";
 
-export default ChartBar
+  if (props.max > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="char-bar__inner">
+        <div className="char-bar__fill" style={{ height: barFillHeight }}></div>
+      </div>
+      <div className="char-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;
