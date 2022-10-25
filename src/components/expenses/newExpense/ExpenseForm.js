41ef7f37612bca5ac553e78b6f8e1d8573f99325
itanglishdev <chrishethertonweb@gmail.com>
2022-10-25 22:19:08 +0200

    last touches and bug fixed

--- src/components/expenses/newExpense/ExpenseForm.js
+++ src/components/expenses/newExpense/ExpenseForm.js
@@ -1,76 +1,81 @@
-import './ExpenseForm.css'
-import { useState } from 'react'
+import "./ExpenseForm.css"
+import { useState } from "react"
 
 function ExpenseForm(props) {
+	const [enteredAmount, setEnteredAmount] = useState("")
+	const [enteredTitle, setEnteredTitle] = useState("")
+	const [enteredDate, setEnteredDate] = useState("")
 
-  const [enteredAmount, setEnteredAmount] = useState('')
-  const [enteredTitle, setEnteredTitle] = useState('')
-  const [enteredDate, setEnteredDate] = useState('')
+	const titleChangeHandler = (event) => {
+		setEnteredTitle(event.target.value)
+	}
 
-  const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value)
-  }
+	const amountChangeHandler = (event) => {
+		setEnteredAmount(event.target.value)
+	}
 
-  const amountChangeHandler = (event) => {
+	const dateChangeHandler = (event) => {
+		setEnteredDate(event.target.value)
+	}
 
-    setEnteredAmount(event.target.value)
-  }
+	const submitHandler = (event) => {
+		event.preventDefault()
 
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value)
-  }
+		const expenseData = {
+			title: enteredTitle,
+			amount: +enteredAmount,
+			date: new Date(enteredDate),
+		}
 
-  const submitHandler = (event) => {
-    event.preventDefault()
+		props.onSaveExpenseData(expenseData)
+		setEnteredAmount("")
+		setEnteredDate("")
+		setEnteredTitle("")
+	}
 
-    const expenseData = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate)
-    }
-
-    props.onSaveExpenseData(expenseData);
-    setEnteredAmount('')
-    setEnteredDate('')
-    setEnteredTitle('')
-  }
-
-  return (
-
-    <form onSubmit={submitHandler} >
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler} />
-        </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input
-            type="number"
-            value={enteredAmount}
-            min='0.01'
-            step='0.01'
-            onChange={amountChangeHandler} />
-        </div>
-        <div className='new-expense__control'>
-          <label >Date</label>
-          <input
-            type="date"
-            value={enteredDate}
-            min='2019-01-01'
-            max='2022-12-31'
-            onChange={dateChangeHandler} />
-        </div>
-      </div>
-      <div className='new-expenses__action' >
-        <button type="button" onClick={props.onCancel} >Cancel</button>
-        <button type="submit">Add expense</button>
-      </div>
-    </form>
-  );
+	return (
+		<form onSubmit={submitHandler}>
+			<div className="new-expense__controls">
+				<div className="new-expense__control">
+					<label>Title</label>
+					<input
+						type="text"
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label>Amount</label>
+					<input
+						type="number"
+						value={enteredAmount}
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandler}
+					/>
+				</div>
+				<div className="new-expense__control">
+					<label>Date</label>
+					<input
+						type="date"
+						value={enteredDate}
+						min="2019-01-01"
+						max="2022-12-31"
+						onChange={dateChangeHandler}
+					/>
+				</div>
+			</div>
+			<div className="new-expenses__action">
+				<button
+					type="button"
+					onClick={props.onCancel}
+				>
+					Cancel
+				</button>
+				<button type="submit">Add expense</button>
+			</div>
+		</form>
+	)
 }
 
-export default ExpenseForm;
+export default ExpenseForm
