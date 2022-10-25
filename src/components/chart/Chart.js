4f80c2c50c8938c0c5164823a4d91f504ac584f2
itanglishdev <chrishethertonweb@gmail.com>
2022-10-25 20:30:00 +0200

    Prettier re-formatted

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,16 +1,18 @@
-import ChartBar from "./ChartBar"
-import './ChartBar.css'
+import ChartBar from "./ChartBar";
+import "./ChartBar.css";
 
 const Chart = (props) => {
-
-
   return (
     <div className="chart">
-           {
-           props.dataPoints.map(dataPoint => <ChartBar value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)
-           }  
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
